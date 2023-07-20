db14b9c9f4846488af295f7a9d6444875c29544a
wzw-999 <63135037+wzw-999@users.noreply.github.com>
2023-07-20 16:32:09 +0800

    Update wnyd.js

--- QuantumultX/File/wnyd.js
+++ QuantumultX/File/wnyd.js
@@ -24,6 +24,6 @@ hostname = p.du.163.com
 var body = $response.body;
 var obj = JSON.parse(body);
 
-obj.tradeEndTime = 4102388593;
+obj.tradeEndTime = 4102388593000;
 body = JSON.stringify(obj);
 $done({body});
